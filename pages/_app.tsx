ad2a2418612cecec6bfc73ea0860b8058a6077cb
yoGhastly <ve06976@innovaccion.mx>
2022-09-12 09:57:04 -0500

    Tailwindcss added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
+// pages/_app.js
+import '../styles/globals.css'
 import { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apollo'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <Component {...pageProps} />
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <h1 className="text-4xl text-blue-800 uppercase">Hola</h1>
   </Layout>
 )
 
